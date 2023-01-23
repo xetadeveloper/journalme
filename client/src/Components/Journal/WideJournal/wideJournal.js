8c65fc5a7c7a233b9e7d0095108717eaa8e3e235
xetadeveloper <jazelogin@hotmail.com>
2023-01-23 17:26:54 +0100

    fix: fix error in rendering journals

--- client/src/Components/Journal/WideJournal/wideJournal.js
+++ client/src/Components/Journal/WideJournal/wideJournal.js
@@ -6,78 +6,77 @@ import SmallButton from '../../Buttons/SmallButton/smallButton';
 import TradeChart from '../../TradeChart/tradeChart';
 
 export default function WideJournal(props) {
-  const { showSmallJournal, openJournalHandler, journal, chartData } = props;
-  // console.log('Journal: ', journal);
+    const { showSmallJournal, openJournalHandler, journal, chartData } = props;
+    console.log('Chart data: ', chartData);
+    // console.log('Journal: ', journal);
 
-  const { journalName, market, winningTrades, losingTrades } = journal;
-  const { balance, journalID } = journal;
+    const { journalName, market, winningTrades, losingTrades } = journal;
+    const { balance, journalID } = journal;
 
-  return (
-    <div
-      className={`
+    return (
+        <div
+            className={`
           flex flex-col grey-bg 
           ${style.journal}          
           ${showSmallJournal && style.smallJournal}`}>
-      {/* Journal Info */}
-      <h3 className={`${style.nameDetail}`}>{journalName}</h3>
-      <div className={`flex ${style.detailHolder}`}>
-        <div
-          className={`flex flex-col align-items-center ${style.journalInfo}`}>
-          <div className={`flex flex-col ${style.journalDetails}`}>
-            <h4 className={style.currencyDetail}>
-              Market: <span>{market} </span>
-            </h4>
-          </div>
-          <div className={`flex flex-col ${style.progressDetails}`}>
-            <h4 className={style.detail}>
-              No. Of Trades <span>{winningTrades + losingTrades}</span>
-            </h4>
-            <h4 className={style.detail}>
-              No. Of Wins <span>{winningTrades}</span>
-            </h4>
-            <h4 className={style.detail}>
-              No. Of Losses <span>{losingTrades}</span>
-            </h4>
-          </div>
-        </div>
+            {/* Journal Info */}
+            <h3 className={`${style.nameDetail}`}>{journalName}</h3>
+            <div className={`flex ${style.detailHolder}`}>
+                <div className={`flex flex-col align-items-center ${style.journalInfo}`}>
+                    <div className={`flex flex-col ${style.journalDetails}`}>
+                        <h4 className={style.currencyDetail}>
+                            Market: <span>{market} </span>
+                        </h4>
+                    </div>
+                    <div className={`flex flex-col ${style.progressDetails}`}>
+                        <h4 className={style.detail}>
+                            No. Of Trades <span>{winningTrades + losingTrades}</span>
+                        </h4>
+                        <h4 className={style.detail}>
+                            No. Of Wins <span>{winningTrades}</span>
+                        </h4>
+                        <h4 className={style.detail}>
+                            No. Of Losses <span>{losingTrades}</span>
+                        </h4>
+                    </div>
+                </div>
 
-        {/* Chart Display */}
-        {!showSmallJournal && (
-          <div className={`${style.chart}`}>
-            {/* Show chart here */}
-            <TradeChart
-              chartData={chartData}
-              chartOptions={{
-                panEnabled: true,
-                chartTitle: {
-                  chartName: 'Trades Analysis',
-                  font: { size: 18, family: 'Arvo' },
-                },
-              }}
-            />
-          </div>
-        )}
+                {/* Chart Display */}
+                {!showSmallJournal && Object.keys(chartData).length && (
+                    <div className={`${style.chart}`}>
+                        {/* Show chart here */}
+                        <TradeChart
+                            chartData={chartData}
+                            chartOptions={{
+                                panEnabled: true,
+                                chartTitle: {
+                                    chartName: 'Trades Analysis',
+                                    font: { size: 18, family: 'Arvo' },
+                                },
+                            }}
+                        />
+                    </div>
+                )}
 
-        {/* Journal Account Details */}
-        <div
-          className={`flex flex-col align-items-center ${style.journalAcct}`}>
-          <h4 className={style.journalBalance}>
-            Balance:
-            <span className={`${balance >= 0 ? style.profit : style.loss}`}>
-              {balance >= 0 ? `$${balance}` : `-$${Math.abs(balance)}`}
-            </span>
-          </h4>
-          <div className={style.openJournalBtn}>
-            <SmallButton
-              btnType='button'
-              btnText='Open Journal'
-              clickHandler={() => {
-                openJournalHandler(journalID);
-              }}
-            />
-          </div>
+                {/* Journal Account Details */}
+                <div className={`flex flex-col align-items-center ${style.journalAcct}`}>
+                    <h4 className={style.journalBalance}>
+                        Balance:
+                        <span className={`${balance >= 0 ? style.profit : style.loss}`}>
+                            {balance >= 0 ? `$${balance}` : `-$${Math.abs(balance)}`}
+                        </span>
+                    </h4>
+                    <div className={style.openJournalBtn}>
+                        <SmallButton
+                            btnType='button'
+                            btnText='Open Journal'
+                            clickHandler={() => {
+                                openJournalHandler(journalID);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
